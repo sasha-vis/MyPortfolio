ab153ba9c68e4a972237e0f5436178b3e68191f3
alex.vysocki <sasha.vysocki@gmail.com>
2022-04-26 20:55:26 +0300

    Version 2.0

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+let navElements = document.querySelectorAll('.another-text_nav > div');
+let contentElements = document.querySelectorAll('.another-text_content > div > div');
+
+navElements.forEach(function(elem){
+    elem.addEventListener('click', function(){
+        if (elem.classList.contains('nav-active')) return;
+        
+        elem.classList.toggle('nav-active');
+        navElements.forEach(navElem => {
+            if (navElem.classList.contains('nav-active') && navElem !== elem) {
+                navElem.classList.toggle('nav-active');
+            }
+        })
+
+        contentElements.forEach(contentElement => {
+            if (contentElement.dataset.id === elem.dataset.id) {
+                contentElement.classList.add('content-active');
+            } else {
+                contentElement.classList.remove('content-active');
+            }
+        });
+    });
+});
+
+let certImages = document.querySelectorAll('.cert-block-wrapper img');
+
+certImages.forEach(function(elem){
+    elem.addEventListener('click', function(event){
+        var el = document.getElementsByTagName('body')[0];
+        el.style.overflow = 'hidden';
+
+        let popup = document.querySelector('.cert-block .popup');
+
+        popup.classList.add('show-popup');
+        popup.firstElementChild.setAttribute('src', event.target.currentSrc);
+    });
+});
+
+let closeBtn = document.querySelector('.cert-block .popup .close-btn');
+
+closeBtn.addEventListener('click', function(){
+    let popup = document.querySelector('.cert-block .popup');
+    popup.classList.remove('show-popup');
+
+    var el = document.getElementsByTagName('body')[0];
+    el.style.overflow = 'auto';
+});
+
+
+
 const accord = function(){
     const buttons = document.querySelectorAll('.accord .title');
 
